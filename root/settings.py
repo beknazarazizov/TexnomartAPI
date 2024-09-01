1655984feb8e850ff53377fec28dab6023a00bc4
beknazar <beknazarbekazizov@gmail.com>
2024-09-01 12:05:38 +0500

    teachers emaill changed

--- root/settings.py
+++ root/settings.py
@@ -129,7 +129,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'beknazarazizov0213@gmail.com'
-TEACHER_EMAIL = 'beknazarbekazizov@gmail.com'
+TEACHER_EMAIL = 'jasurmavlonov24@gmail.com'
 EMAIL_HOST_PASSWORD = 'ekzl ublq pnrf rwtp'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
